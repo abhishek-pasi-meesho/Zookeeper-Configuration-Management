ebe8fcb8648cb3270672072dea8a8db063d714f1
abhishek-pasi-meesho <abhishek.pasi@meesho.com>
2020-04-13 16:03:46 +0530

    @Value removed

--- src/main/java/com/meesho/zookeeper/config/DynamicNameConfig.java
+++ src/main/java/com/meesho/zookeeper/config/DynamicNameConfig.java
@@ -1,19 +1,18 @@
 package com.meesho.zookeeper.config;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@ConfigurationProperties
 @PropertySource("classpath:application.properties")
 @Data
 @RefreshScope
 public class DynamicNameConfig {
-    @Value("${myName}")
     private String myName;
 
-    @Value("${address}")
     private String address;
 }
